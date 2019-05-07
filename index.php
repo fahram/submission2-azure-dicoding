b0b0631c66023a2045e12c5b8c528755f8b699c4
Khaidir Fahram <khaidir@fahram.com>
2019-05-07 14:45:09 +0700

    ssl problem

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ $fileToUpload = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dicoding Azure Submission</title>
-    <script src="httpss://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
